3bfe7b8f2e3fcacbeb372e7673c7514274626cdf
Thiago do N. Ferreira <thiagodnf@gmail.com>
2022-07-17 10:46:12 -0400

    test

--- dist/just.bundle.js
+++ dist/just.bundle.js
@@ -128,10 +128,17 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 __webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "myfunc": () => (/* binding */ myfunc)
+/* harmony export */ });
 /* harmony import */ var _utils_csv_util__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 /* harmony import */ var _utils_random_utils__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2);
 
 
+
+function myfunc(){
+    console.log("oi");
+}
 
 })();
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import CSVUtils from "./utils/csv-util";
 import RandomUtils from "./utils/random-utils";
+
+export function myfunc(){
+    console.log("oi");
+}
